52f5b36556da40b409eaeae00f5eeeae429c5f5c
Daniel DePaoli <dannydepaoli0@gmail.com>
2020-11-16 13:58:36 -0800

    Webpack debugging

--- Server/index.js
+++ Server/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const path = require("path");
-const app = require("express");
+const app = express();
 const port = 3100;
 
 app.use(express.json());
-app.use(express.static(path.join(_dirname,"../client/dist")));
+app.use(express.static(path.join(__dirname,"../client/dist")));
 
-app.listen(Port, (err) => {
+app.listen(port, (err) => {
 if(err){
   console.log(err);
 } else {
