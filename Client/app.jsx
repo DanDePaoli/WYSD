85e1b54e208103512d8a55e2e6f2ab5949e921d7
Daniel DePaoli <dannydepaoli0@gmail.com>
2020-11-16 13:29:24 -0800

    Built out App

--- Client/app.jsx
+++ Client/app.jsx
@@ -2,7 +2,21 @@ import react from 'react';
 import axios from 'axios';
 
 class App extends React.Component {
+constructor(props){
+super(props);
+this.state = {}
 
+}
+
+render () {
+
+
+  return (
+<div>React is Rendering</div>
+
+  );
+
+}
 
 
 }
