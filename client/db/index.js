c846674f66beaec3d039262096ddea38420691d9
Daniel DePaoli <dannydepaoli0@gmail.com>
2020-11-18 18:47:20 -0800

    DB index update

---
+++ client/db/index.js
@@ -0,0 +1,15 @@
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'bookingCalendar'
+});
+
+connection.connect((err) => {
+  if(err) {
+    console.log(err);
+  } else {
+    console.log('Connected to DB')
+  }
+});
+
+module.exports = connection;
