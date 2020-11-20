25932e542ddb3c167a6bda1862ad8fd9523b0e53
Daniel DePaoli <dannydepaoli0@gmail.com>
2020-11-20 14:57:06 -0800

    Seeding script

---
+++ client/db/seed.js
@@ -0,0 +1,21 @@
+const db = require('./index.js');
+
+
+// db.query(`insert into rentals (rental_name, rental_description, rating, guestMax, pricePerNight, discountPricePerNight, weeklyDiscount, cleaningFee, serviceFee, numOfReviews) values ("${randomRentalName}", "${randomDescription}", ${randomRating}, ${randomGuestMax}, ${randomPricePerNight}, ${randomDiscountedPricePerNight}, ${randomHasWeeklyDiscount}, ${randomCleaningFee}, ${randomServiceFee}, ${randomnumOfReviews})`, (err, results, fields) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(null, results);
+//   }
+// })
+// }
+
+
+// var generateRentals = (num) => {
+// for (var i=0; i < num; i++){
+// generateRental();
+// }
+// }
+
+// generateRentals(5);
+
